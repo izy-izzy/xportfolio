73b8851566eff26485394f10c4d823c3d31bff96
izy-izzy <LukasKalcok@LUKASKALCOK01.q2cz.local>
2015-11-20 17:39:27 +0100

    apiary test sucessfull

--- devel/scripts/controllers/introcontroller.js
+++ devel/scripts/controllers/introcontroller.js
@@ -1,3 +1,9 @@
-xportfolio.controller("introController", function($scope) {
+xportfolio.controller("introController", function($scope, apiaryConnectionService) {
+	apiaryConnectionService.getTestData()
+	.then(function (response) {
+		console.log("data:", response);
+	}, function (response) {
+		console.log("Unable to load test data.");
+	});
     console.log("intro");
 });
